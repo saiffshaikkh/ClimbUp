ab89e4fd3031b2eb7d83f0b064ca4a7ecc647126
Saif <callmesaif414@gmail.com>
2026-01-01 01:54:25 +0530

    Completed Theme SetuUp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 import { SignInButton, UserButton } from "@clerk/nextjs";
-import Image from "next/image";
+import { ThemeToggle } from "@/components/ui/ThemeToggle";
 
 export default function Home() {
   return (
     <>
-      <SignInButton />
-      <UserButton />
+      <div className="p-4 space-y-4">
+        <div className="flex items-center gap-4">
+          <SignInButton />
+          <UserButton />
+          <ThemeToggle />
+        </div>
+      </div>
     </>
   );
 }
